0455e1d09bdb85403fdc4ccbeb9eb6cc4e0b509c
iordakis <iordakis@gmail.com>
2021-06-23 04:12:03 +0300

    Create find-missing-integer.py

---
+++ find-missing-integer.py
@@ -0,0 +1,17 @@
+# https://github.com/iordakis/
+# issue: A simple iterative way of finding any missing integer in a sorted array
+import numpy as np
+
+# example array with missing integers (a list can also be used)
+array = np.array([1,2,4,5,6,7,8,9,11,12,15,19,33,35])
+
+C = 0
+incr = 1
+
+for x in range(len(array)-1):
+    if array[C] - array[C+1] <= -1:
+        for x in range(array[C+1] - array[C] - 1):
+            print(array[C]+incr, 'is missing')
+            incr = incr +1
+        incr = 1
+    C = C+1
